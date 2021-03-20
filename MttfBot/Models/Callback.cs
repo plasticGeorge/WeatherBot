51e93e9e68b0a82357a85695214caddec01ea0ad
PlasticGeorge <georgylemko@yandex.ru>
2021-03-21 00:05:45 +0100

    ~~~~6

--- MttfBot/Models/Callback.cs
+++ MttfBot/Models/Callback.cs
@@ -29,38 +29,38 @@ namespace MttfBot.Models
         public override string ToString()
         {
 			return
-			$"type: {this.Type}\n" +
+			$"type: {this?.Type}\n" +
 			$"object:\n" +
 			$"\tmessage:\n" +
-			$"\t\tdate: {this.Object.Message.Date}" +
-			$"\t\tfrom_id: {this.Object.Message.FromId}" +
-			$"\t\tid: {this.Object.Message.Id}" +
-			$"\t\tout: {this.Object.Message.Out}" +
-			$"\t\tpeer_id: {this.Object.Message.PeerId}" +
-			$"\t\ttext: {this.Object.Message.Text}" +
-			$"\t\tconversation_message_id: {this.Object.Message.ConversationMessageId}" +
+			$"\t\tdate: {this.Object?.Message?.Date}" +
+			$"\t\tfrom_id: {this.Object?.Message?.FromId}" +
+			$"\t\tid: {this.Object?.Message?.Id}" +
+			$"\t\tout: {this.Object?.Message?.Out}" +
+			$"\t\tpeer_id: {this.Object?.Message?.PeerId}" +
+			$"\t\ttext: {this.Object?.Message?.Text}" +
+			$"\t\tconversation_message_id: {this.Object?.Message?.ConversationMessageId}" +
 			$"\t\tfwd_messages: -" +
-			$"\t\timportant: {this.Object.Message.Important}" +
-			$"\t\trandom_id: {this.Object.Message.RandomId}" +
+			$"\t\timportant: {this.Object?.Message?.Important}" +
+			$"\t\trandom_id: {this.Object?.Message?.RandomId}" +
 			$"\t\tattachments: -" +
-			$"\t\tis_hidden: {this.Object.Message.IsHidden}" +
+			$"\t\tis_hidden: {this.Object?.Message?.IsHidden}" +
 			$"\tclient_info:" +
 			$"\t\tbutton_actions: [" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[0]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[1]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[2]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[3]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[4]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[5]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[6]}" +
-			$"\t\t\t{this.Object.ClientInfo.ButtonActions[7]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[0]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[1]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[2]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[3]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[4]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[5]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[6]}" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[7]}" +
 			$"\t\t\t]" +
-			$"\t\tkeyboard: {this.Object.ClientInfo.Keyboard}" +
-			$"\t\tinline_keyboard: {this.Object.ClientInfo.InlineKeyboard}" +
-			$"\t\tcarousel: {this.Object.ClientInfo.Carousel}" +
-			$"\t\tlang_id: {this.Object.ClientInfo.LangId}" +
-			$"group_id: {this.GroupId}" +
-			$"group_id: {this.EventID}";
+			$"\t\tkeyboard: {this.Object?.ClientInfo?.Keyboard}" +
+			$"\t\tinline_keyboard: {this.Object?.ClientInfo?.InlineKeyboard}" +
+			$"\t\tcarousel: {this.Object?.ClientInfo?.Carousel}" +
+			$"\t\tlang_id: {this.Object?.ClientInfo?.LangId}" +
+			$"group_id: {this?.GroupId}" +
+			$"group_id: {this?.EventID}";
         }
     }
 }
