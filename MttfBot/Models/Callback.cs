26dda41fedf546018a057b9d615b89ac25e3d624
PlasticGeorge <georgylemko@yandex.ru>
2021-03-20 16:57:16 +0100

    changed Callback model

--- MttfBot/Models/Callback.cs
+++ MttfBot/Models/Callback.cs
@@ -14,12 +14,12 @@ namespace MttfBot.Models
         public string Type { get; set; }
 
         [JsonProperty("object")]
-        public JToken Object { get; set; }
+        public JObject Object { get; set; }
 
         [JsonProperty("group_id")]
         public long GroupId { get; set; }
 
-        [JsonProperty("secret")]
-        public string Secret { get; set; }
+        [JsonProperty("event_id")]
+        public string EventId { get; set; }
     }
 }
