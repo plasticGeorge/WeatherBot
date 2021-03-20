b21f6dcc1d1a04d98163c4cdbbc7c91ca3db79f3
PlasticGeorge <georgylemko@yandex.ru>
2021-03-20 16:40:48 +0100

    start

---
+++ MttfBot/Controllers/CallbackController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VkNet.Abstractions;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Utils;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MttfBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallbackController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        private IVkApi _api;
+        public CallbackController(IConfiguration configuration, IVkApi api)
+        {
+            _configuration = configuration;
+            _api = api;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Models.Callback callback)
+        {
+            switch(callback.Type)
+            {
+                case "confirmation":
+                    return Ok(_configuration["Confirmation:" + callback.GroupId.ToString()]);
+                case "new_message":
+                    var mes = Message.FromJson(new VkResponse(callback.Object));
+                    _api.Messages.Send(new MessagesSendParams
+                    {
+                        RandomId = DateTime.Now.Millisecond,
+                        PeerId = mes.PeerId,
+                        Message = mes.Text
+                    });
+                    return Ok("ok");
+                default:
+                    return Ok("ok");
+            };
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Redirect("https://www.youtube.com/watch?v=eiqfAdyCEGY");
+        }
+    }
+}
---
+++ MttfBot/Models/Callback.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MttfBot.Models
+{
+    [Serializable]
+    public class Callback
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("object")]
+        public JToken Object { get; set; }
+
+        [JsonProperty("group_id")]
+        public long GroupId { get; set; }
+
+        [JsonProperty("secret")]
+        public string Secret { get; set; }
+    }
+}
