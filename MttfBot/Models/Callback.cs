bfc5f1ebe6e0ca80e6c493dfe4f0d6dd4d7fae85
PlasticGeorge <georgylemko@yandex.ru>
2021-03-21 00:09:08 +0100

    ~~~~7

--- MttfBot/Models/Callback.cs
+++ MttfBot/Models/Callback.cs
@@ -26,41 +26,41 @@ namespace MttfBot.Models
 		[JsonProperty("secret")]
 		public string Secret { get; set; }
 
-        public override string ToString()
-        {
+		public override string ToString()
+		{
 			return
 			$"type: {this?.Type}\n" +
 			$"object:\n" +
 			$"\tmessage:\n" +
-			$"\t\tdate: {this.Object?.Message?.Date}" +
-			$"\t\tfrom_id: {this.Object?.Message?.FromId}" +
-			$"\t\tid: {this.Object?.Message?.Id}" +
-			$"\t\tout: {this.Object?.Message?.Out}" +
-			$"\t\tpeer_id: {this.Object?.Message?.PeerId}" +
-			$"\t\ttext: {this.Object?.Message?.Text}" +
-			$"\t\tconversation_message_id: {this.Object?.Message?.ConversationMessageId}" +
-			$"\t\tfwd_messages: -" +
-			$"\t\timportant: {this.Object?.Message?.Important}" +
-			$"\t\trandom_id: {this.Object?.Message?.RandomId}" +
-			$"\t\tattachments: -" +
-			$"\t\tis_hidden: {this.Object?.Message?.IsHidden}" +
-			$"\tclient_info:" +
-			$"\t\tbutton_actions: [" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[0]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[1]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[2]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[3]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[4]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[5]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[6]}" +
-			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[7]}" +
-			$"\t\t\t]" +
-			$"\t\tkeyboard: {this.Object?.ClientInfo?.Keyboard}" +
-			$"\t\tinline_keyboard: {this.Object?.ClientInfo?.InlineKeyboard}" +
-			$"\t\tcarousel: {this.Object?.ClientInfo?.Carousel}" +
-			$"\t\tlang_id: {this.Object?.ClientInfo?.LangId}" +
-			$"group_id: {this?.GroupId}" +
-			$"group_id: {this?.EventID}";
-        }
-    }
+			$"\t\tdate: {this.Object?.Message?.Date}\n" +
+			$"\t\tfrom_id: {this.Object?.Message?.FromId}\n" +
+			$"\t\tid: {this.Object?.Message?.Id}\n" +
+			$"\t\tout: {this.Object?.Message?.Out}\n" +
+			$"\t\tpeer_id: {this.Object?.Message?.PeerId}\n" +
+			$"\t\ttext: {this.Object?.Message?.Text}\n" +
+			$"\t\tconversation_message_id: {this.Object?.Message?.ConversationMessageId}\n" +
+			$"\t\tfwd_messages: -\n" +
+			$"\t\timportant: {this.Object?.Message?.Important}\n" +
+			$"\t\trandom_id: {this.Object?.Message?.RandomId}\n" +
+			$"\t\tattachments: -\n" +
+			$"\t\tis_hidden: {this.Object?.Message?.IsHidden}\n" +
+			$"\tclient_info:\n" +
+			$"\t\tbutton_actions: [\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[0]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[1]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[2]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[3]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[4]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[5]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[6]}\n" +
+			$"\t\t\t{this.Object?.ClientInfo?.ButtonActions[7]}\n" +
+			$"\t\t\t]\n" +
+			$"\t\tkeyboard: {this.Object?.ClientInfo?.Keyboard}\n" +
+			$"\t\tinline_keyboard: {this.Object?.ClientInfo?.InlineKeyboard}\n" +
+			$"\t\tcarousel: {this.Object?.ClientInfo?.Carousel}\n" +
+			$"\t\tlang_id: {this.Object?.ClientInfo?.LangId}\n" +
+			$"group_id: {this?.GroupId}\n" +
+			$"event_id: {this?.EventID}\n";
+		}
+	}
 }
