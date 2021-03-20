134b9779b7a94114e01cc84e32c4c6ebdcee2d8c
PlasticGeorge <georgylemko@yandex.ru>
2021-03-20 18:05:21 +0100

    changed type of property Object in model Callback on JContainer

--- MttfBot/Models/Callback.cs
+++ MttfBot/Models/Callback.cs
@@ -14,7 +14,7 @@ namespace MttfBot.Models
 		public string Type { get; set; }
 
 		[JsonProperty("object")]
-		public JObject Object { get; set; }
+		public JContainer Object { get; set; }
 
 		[JsonProperty("group_id")]
 		public long GroupId { get; set; }
