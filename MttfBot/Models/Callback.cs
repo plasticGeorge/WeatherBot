5a4cdc21e287f37b3bb2038aea014bc93f004800
PlasticGeorge <georgylemko@yandex.ru>
2021-03-20 19:38:54 +0100

    @d8#Q23p=4$.rp3

--- MttfBot/Models/Callback.cs
+++ MttfBot/Models/Callback.cs
@@ -12,17 +12,5 @@ namespace MttfBot.Models
 	{
 		[JsonProperty("type")]
 		public string Type { get; set; }
-
-		[JsonProperty("object")]
-		public JObject Object { get; set; }
-
-		[JsonProperty("group_id")]
-		public long GroupId { get; set; }
-
-		[JsonProperty("event_id")]
-		public string EventID { get; set; }
-
-		[JsonProperty("secret")]
-		public string Secret { get; set; }
 	}
 }
