4cc75f46641a63d10bf1590b6284eecf2851153f
PlasticGeorge <georgylemko@yandex.ru>
2021-03-26 15:21:09 +0100

    another fix -_-

--- MttfBot/Startup.cs
+++ MttfBot/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MttfBot.Bots;
-using MttfBot.HelperClasses;
 using MttfBot.Interfaces;
 using MttfBot.WeatherForecasters;
 using Newtonsoft.Json.Serialization;
